b438c9050cf1d7175350fad1b42a09bf78f3dbe6
yasmen zuheer <yasibrahem9@gmail.com>
2024-08-19 11:34:31 +0300

    add navbar and routes

---
+++ new-project/resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+       Contact Page
+    </x-slot:heading>
+    <h1>hello from contact page</h1>
+</x-layout>
---
+++ new-project/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h1>hello from home page yasmen</h1>
+</x-layout>
